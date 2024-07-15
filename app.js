ce812efb30eda7d644dbe60299afeb4e270296b4
tarikaytek <aytek.tarik1@gmail.com>
2024-07-15 22:55:17 +0300

    add static files

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+//MIDDLEWARES
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+})
+
+
+
+//bu da bi middleware yaani
+/* app.get('/', (req, res) => {
 
     const photo = {
         id: 1,
@@ -12,7 +21,10 @@ app.get('/', (req, res) => {
         description: "photo desc 1",
     }
     res.send(photo);
-});
+}); */
+
+
+
 
 const port = 3000;
 app.listen(port, () => {
