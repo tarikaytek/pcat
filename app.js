fd8ebadc4a2429e8f509ed0820896f2e2458c8f1
tarikaytek <aytek.tarik1@gmail.com>
2024-07-15 21:21:15 +0300

    create framework

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const fn = function () {
-    console.log('anan');
-}
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+
+    const photo = {
+        id: 1,
+        name: "photo1",
+        description: "photo desc 1",
+    }
+    res.send(photo);
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Sunucu ${port} adresinde baslatildi.`);
+});
